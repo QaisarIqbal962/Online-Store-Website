fba937ccd5e718f9f8d9277fe9637217ff2587bb
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-08 22:00:11 +0500

    add data in shop context

--- Online_Store/src/App.jsx
+++ Online_Store/src/App.jsx
@@ -13,7 +13,31 @@ import bannerKids from "./Components/Assets/banner_kids.png";
 
 const App = () => {
   return (
-    
+    <BrowserRouter>
+      <ShopContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/men"
+            element={<ShopCategory category="men" banner={bannerMen} />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory category="women" banner={bannerWomen} />}
+          />
+          <Route
+            path="/kid"
+            element={<ShopCategory category="kid" banner={bannerKids} />}
+          />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+          <Route path="*" element={<p>Page not found.</p>} />
+        </Routes>
+        <Footer />
+      </ShopContextProvider>
+    </BrowserRouter>
   );
 };
 
